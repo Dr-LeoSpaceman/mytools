f8eb007166b4f5b4ab708e0f6b2d4a524ff4a76f
vincent <vincent@seculabs.ch>
2022-03-18 09:58:30 +0100

    my tools commit

---
+++ getstats.py
@@ -0,0 +1,28 @@
+import mysql.connector as mysql
+from os import environ
+
+db = mysql.connect(
+    host = "localhost",
+    user = "root",
+    passwd = "Sup3rDup3rS3cr3t#",
+    database="dev"
+)
+conn = db.cursor()
+query = "SELECT * FROM devdata;"
+
+logs = []
+with conn as cur:
+    cur.execute(query)
+    results = cur.fetchall()
+    for data in results:
+        email = data[1]
+        success = data[2]
+        failed = data[3]
+        get_total = f"total = ({success} + {failed}) / {failed}"
+        exec(get_total, globals())
+        logs.append(f"User {email} -> {total}")
+
+
+with open("/tmp/logs.log", "w") as f:
+    for log in logs:
+        f.write(f"{log}\n")
